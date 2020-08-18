28eb26d1563d954a0607d5e59448498a6eb1030e
popany <popany@163.com>
2020-08-18 13:24:19 +0000

    add

---
+++ use_odp.net/use_unmanaged_driver/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace use_unmanaged_driver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UnManagedDatabaseAccessor unManagedDatabaseAccessor = new UnManagedDatabaseAccessor();
+            unManagedDatabaseAccessor.WriteTable();
+        }
+    }
+}
