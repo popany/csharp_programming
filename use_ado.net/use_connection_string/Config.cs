9b26512f2983cdfacb709908dbf4451b668e4fa0
popany <popany@163.com>
2020-09-17 09:34:45 +0000

    add

---
+++ use_ado.net/use_connection_string/Config.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace use_connection_string
+{
+    class Config
+    {
+        private static readonly Config instance = new Config();
+
+        private Config()
+        {
+            Reload();
+        }
+
+        public static Config Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public void Reload()
+        {
+            Configuration configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var appSettings = configuration.AppSettings;
+
+            connectionString = appSettings.Settings["connection_string"].Value.ToString();
+        }
+
+        static public string ConnectionString
+        {
+            get
+            {
+                return Instance.connectionString;
+            }
+        }
+
+        public string connectionString;
+    }
+}
---
+++ use_ado.net/use_connection_string/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// https://docs.microsoft.com/en-us/dotnet/framework/data/adonet/connection-strings
+namespace use_connection_string
+{
+    class Program
+    {
+        static void ShowDataSource()
+        {
+            using (SqlConnection connection = new SqlConnection(Config.ConnectionString))
+            {
+                connection.Open();
+                Console.WriteLine("ServerVersion: {0}", connection.ServerVersion);
+                Console.WriteLine("DataSource: {0}", connection.DataSource);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ShowDataSource();
+            Console.Read();
+        }
+    }
+}
